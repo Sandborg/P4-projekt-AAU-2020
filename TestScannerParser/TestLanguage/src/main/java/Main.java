2972afc8210b8edf37292a0c40b2206316a27042
Maggedelle <33657258+Maggedelle@users.noreply.github.com>
2021-05-22 15:55:57 +0200

    Last changes

--- TestScannerParser/TestLanguage/src/main/java/Main.java
+++ TestScannerParser/TestLanguage/src/main/java/Main.java
@@ -6,8 +6,10 @@ public class Main {
         //Syntax Analysis
         parser p = new parser(new Scanner(new FileReader(args[0])));
         p.parse();
+
         //Write AST
         p.action_obj.WriteAST("ast.json");
+
         // Contextual Analysis
         SymbolTable global = new SymbolTable(null);
         Analyzer analyzer = new Analyzer((ProgramNode)p.action_obj.prog,global);
